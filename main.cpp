6f7f6a6c859347a57c8781bef58fdbb88b810095
jessica <85281447+imageoracle@users.noreply.github.com>
2024-01-23 19:53:45 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main01()
+{
+    cout << "Hello world." << endl;
+    int num1 = 23;
+    int num2 = 55;
+    int sum = 0;
+
+    sum = num1 + num2;
+
+    cout << sum;
+    return 0;
+}
